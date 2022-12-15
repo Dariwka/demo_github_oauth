1418f2e37d3bf55360ce17e1c55659201e2865c0
Dariwka <77488950+Dariwka@users.noreply.github.com>
2022-12-15 11:46:08 +0200

    added api folder and main installation express,nodemon, cookie-session, cors, passport

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <Navbar />
+        <Navbar user={user} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
   return (
     <div className="navbar">
       <span className="logo">
         <Link className="link" to="/">
           Diana's App
         </Link>
-      </span>
-      <ul className="list">
-        <li className="listItem">
-          <img
-            src="https://res.cloudinary.com/lvimeridijan/image/upload/v1671023693/me/77b0236a-e6d0-49cd-96be-69fefbcb7b5b_trluvr.jpg"
-            alt=""
-            className="avatar"
-          />
-        </li>
-        <li className="listItem">John Doe</li>
-        <li className="listItem">Logout</li>
-      </ul>
+      </span>{" "}
+      {user ? (
+        <ul className="list">
+          <li className="listItem">
+            <img
+              src="https://res.cloudinary.com/lvimeridijan/image/upload/v1671023693/me/77b0236a-e6d0-49cd-96be-69fefbcb7b5b_trluvr.jpg"
+              alt=""
+              className="avatar"
+            />
+          </li>
+          <li className="listItem">John Doe</li>
+          <li className="listItem">Logout</li>
+        </ul>
+      ) : (
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      )}
     </div>
   );
 };
